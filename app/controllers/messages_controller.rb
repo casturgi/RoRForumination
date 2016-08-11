1a82958befa3b70f603177019984276cf262c574
Cory Sturgis-Casturgi <casturgi@gmail.com>
2016-08-11 15:48:19 -0400

    created new user using devise

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,6 @@
 class MessagesController < ApplicationController
+	before_action :find_message, only: [:show, :edit, :update, :destroy]
+
 	def index
 		@messages = Message.all.order("created_at DESC")
 	end
@@ -17,23 +19,33 @@ class MessagesController < ApplicationController
 		@message = Message.new
 	end
 
+	def show
+	end
+
 	def edit
 	end
 
-	def show
-		@message = Message.find(params[:id])
-	end
-
 	def update
+		if @message.update(message_params)
+			redirect_to message_path
+		else
+			render 'edit'
+		end
 	end
 
 	def destroy
+		@message.destroy
+		redirect_to root_path
 	end
 
 	private 
 
 	def message_params
 		params.require(:message).permit(:title, :description)
+	end
+
+	def find_message
+		@message = Message.find(params[:id])
 	end
 
 end
