d0a6fa0acc67899b12b48ba2f7f333ec6fcce85a
Cory Sturgis-Casturgi <casturgi@gmail.com>
2016-08-11 14:52:23 -0400

    added ability to create new messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,37 @@
 class MessagesController < ApplicationController
 	def index
 	end
+
+	def create 
+		@message = Message.new(message_params)
+
+		if @message.save
+			redirect_to root_path
+		else
+			render 'new'
+		end
+	end
+
+	def new
+		@message = Message.new
+	end
+
+	def edit
+	end
+
+	def show
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+	private 
+
+	def message_params
+		params.require(:message).permit(:title, :description)
+	end
+
 end
